dcbcb1c1d31dbeb8e12d17a886086d18dc1b9b0a
Chamarti Sreshtha <chamartisreshtha123@gmail.com>
2025-01-05 16:26:06 +0530

    Update ChatBot.py

--- ChatBot.py
+++ ChatBot.py
@@ -8,6 +8,7 @@ import streamlit as st
 import random
 from sklearn.feature_extraction. text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
+import joblib
 
 ssl._create_default_https_context = ssl._create_unverified_context
 nltk.data.path.append(os.path.abspath ("nltk_data"))
